9c3d35ae8b065dbeef53fc8908f48fc8a31554e2
baconandegg <pahulank@gmail.com>
2013-11-15 23:56:39 +0900

    ForStudyCpp

---
+++ TestCPP/src/TestCPP.cpp
@@ -0,0 +1,25 @@
+//============================================================================
+// Name        : TestCPP.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "testCPP!!" << endl;
+
+	for(int i = 0; i < 10; i++)
+	{
+		cout << "index : " << i << endl;
+	}
+
+	return 0;
+}
+*/
